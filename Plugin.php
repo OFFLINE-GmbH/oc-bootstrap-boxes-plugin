f414563a4be30ef8b57f8475b0add2fbbd13ca8c
Tobias Kündig <tobias@offline.swiss>
2022-07-09 11:14:52 +0200

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,68 @@
+<?php namespace OFFLINE\BootstrapBoxes;
+
+use Cms\Classes\Controller;
+use October\Rain\Support\Facades\Event;
+use OFFLINE\BootstrapBoxes\Models\TestModel;
+use OFFLINE\Boxes\Classes\Events;
+use OFFLINE\Boxes\Models\Box;
+use System\Classes\PluginBase;
+use System\Models\File;
+
+/**
+ * Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    public $require = ['OFFLINE.Boxes'];
+
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'BootstrapBoxes',
+            'description' => 'No description provided yet...',
+            'author'      => 'OFFLINE',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        Box::extend(function (Box $box) {
+            $box->attachOne['logo'] = File::class;
+            $box->addDynamicMethod('getIconNameOptions', function() {
+                return include('icons.php');
+            });
+        });
+
+        Event::listen(Events::REGISTER_PARTIAL_PATH, function () {
+            return [
+                '$/plugins/offline/bootstrapboxes/partials',
+            ];
+        });
+
+        Event::listen('cms.page.init', function (Controller $controller) {
+            $controller->addJs('https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js', [
+                'integrity' => 'sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2',
+                'crossorigin' => 'anonymous',
+                'async' => 'async',
+            ]);
+
+            $controller->addCss('/plugins/offline/bootstrapboxes/assets/css/custom.css');
+            $controller->addCss('https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css', [
+                'integrity' => 'sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor',
+                'crossorigin' => 'anonymous',
+            ]);
+            $controller->addCss('https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css');
+        });
+    }
+}
---
+++ lang/en/lang.php
@@ -0,0 +1,31 @@
+<?php
+return [
+    'logo' => 'Logo',
+    'title' => 'Title',
+    'text' => 'Text',
+    'content' => 'Content',
+    'buttons' => 'Buttons',
+    'icon' => 'Icon',
+    'primary_button_text' => 'Primary Button Text',
+    'primary_button_href' => 'Primary Button Link Target',
+    'secondary_button_text' => 'Secondary Button Text',
+    'secondary_button_href' => 'Secondary Button Link Target',
+    'logo_height' => 'Logo Height (px)',
+    'image' => 'Image',
+    'image_width' => 'Image Width (px)',
+    'cta_text' => 'CTA Text',
+    'cta_href' => 'CTA Link Target',
+    'hero' => [
+        'section' => 'Hero',
+        'centered' => 'Hero: Centered',
+        'left' => 'Hero: Left aligned',
+    ],
+    'feature' => [
+        'section' => 'Feature',
+        'features' => 'Features',
+        'with_icon' => 'Feature: With Icon',
+        'hanging_icon' => 'Feature: Hanging Icon',
+        'cards' => 'Feature: Cards',
+        'icon_grid' => 'Feature: Icon Grid',
+    ]
+];
